91e236b42773670668dab04c562361ff65d425a4
Pinlun <lunwaac@gmail.com>
2019-11-07 15:32:23 +0800

    Initial Commit

---
+++ Calculator-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator-SwiftUI
+//
+//  Created by Pinlun on 2019/11/7.
+//  Copyright © 2019 Pinlun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
