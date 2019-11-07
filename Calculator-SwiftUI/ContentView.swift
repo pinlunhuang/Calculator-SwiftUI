bbe5d58350be4d390eb34f33d6e9fc4879979d46
Pinlun <lunwaac@gmail.com>
2019-11-07 16:37:10 +0800

    Extract CalculatorButton Subview

--- Calculator-SwiftUI/ContentView.swift
+++ Calculator-SwiftUI/ContentView.swift
@@ -10,12 +10,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            
+            Button(action: {
+                print("Button: +")
+            }) {
+                Text("+")
+                    .font(.system(size: 38))
+                    .foregroundColor(.white)
+                    .frame(width: 88, height: 88)
+                    .background(Color("operatorBackground"))
+                    .cornerRadius(44)
+            }
+            
+            Button(action: {
+                print("Button: +")
+            }) {
+                Text("+")
+                    .font(.system(size: 38))
+                    .foregroundColor(.white)
+                    .frame(width: 88, height: 88)
+                    .background(Color("operatorBackground"))
+                    .cornerRadius(44)
+            }
+            
+            Button(action: {
+                print("Button: +")
+            }) {
+                Text("+")
+                    .font(.system(size: 38))
+                    .foregroundColor(.white)
+                    .frame(width: 88, height: 88)
+                    .background(Color("operatorBackground"))
+                    .cornerRadius(44)
+            }
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalculatorButton: View {
+    
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width/2)
+        }
     }
 }
